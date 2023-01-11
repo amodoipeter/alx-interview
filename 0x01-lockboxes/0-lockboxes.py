9d77e0a8a5bd607c57d1e4f226528ffc8f68636b
Amodoi Peter <amodoipeter@gmail.com>
2023-01-11 04:08:30 +0300

    lockboxes challenge

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Lockboxes challenge"""
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """Determines whether all the boxes can be opened or not
+    '''determines if all the boxes can be opened or not
     Returns:
-    True: all boxes can be opened
-    False: not all the boxes can be opened
-    """
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
     length = len(boxes)
     keys = set()
     opened_boxes = []
     i = 0
+
     while i < length:
         oldi = i
         opened_boxes.append(i)
@@ -20,12 +21,12 @@ def canUnlockAll(boxes):
             if key != 0 and key < length and key not in opened_boxes:
                 i = key
                 break
-            if oldi != i:
-                continue
-            else:
-                break
-            
-            for i in range(length):
-                if i not in opened_boxes and i != 0:
-                    return False
-                return True
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
